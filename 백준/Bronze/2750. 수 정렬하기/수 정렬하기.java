0fcf13d0d093a67b21e16d08e0bc983afb992e0c
박현도 <117895948+hyundo0328@users.noreply.github.com>
2023-02-06 00:38:03 +0900

    [Bronze II] Title: 수 정렬하기, Time: 156 ms, Memory: 14712 KB -BaekjoonHub

--- 백준/Bronze/2750. 수 정렬하기/수 정렬하기.java
+++ 백준/Bronze/2750. 수 정렬하기/수 정렬하기.java
@@ -3,6 +3,9 @@ import java.io.BufferedWriter;
 import java.io.IOException;
 import java.io.InputStreamReader;
 import java.io.OutputStreamWriter;
+import java.util.ArrayList;
+import java.util.Collection;
+import java.util.Collections;
 
 public class Main {
 
@@ -10,25 +13,17 @@ public class Main {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 		BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
 		
-		int t = Integer.parseInt(br.readLine());
-		int arr[] = new int[t];
+		ArrayList<Integer> arr = new ArrayList<>();
 		
-		for(int i=0;i<t;i++) {
-			int num = Integer.parseInt(br.readLine());
-			arr[i] = num;
+		int num = Integer.parseInt(br.readLine());
+		for(int i=0;i<num;i++) {
+			arr.add(Integer.parseInt(br.readLine()));
 		}
 		
-		int i=0,j=0;
-		for(i=1;i<t;i++) {
-			int tmp = arr[i];
-			for(j=i;j>0&&arr[j-1]>tmp;j--) {
-				arr[j] = arr[j-1];
-			}
-			arr[j] = tmp;
-		}
+		Collections.sort(arr);
 		
-		for(i=0;i<t;i++) {
-			bw.write(arr[i]+"\n");
+		for(int i=0;i<num;i++) {
+			bw.write(arr.get(i)+"\n");
 		}
 		
 		br.close();
