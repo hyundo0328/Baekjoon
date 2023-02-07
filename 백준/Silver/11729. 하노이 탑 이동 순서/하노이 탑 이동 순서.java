080f952b2932c83a62aa46285a8a0b95f4d3c917
박현도 <117895948+hyundo0328@users.noreply.github.com>
2023-02-07 14:43:31 +0900

    [Silver I] Title: 하노이 탑 이동 순서, Time: 520 ms, Memory: 73864 KB -BaekjoonHub

--- 백준/Silver/11729. 하노이 탑 이동 순서/하노이 탑 이동 순서.java
+++ 백준/Silver/11729. 하노이 탑 이동 순서/하노이 탑 이동 순서.java
@@ -6,37 +6,33 @@ import java.io.OutputStreamWriter;
 
 public class Main {
 
-	public static StringBuilder sb = new StringBuilder();
+	static BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 	static BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
-	 
+	public static StringBuilder sb = new StringBuilder();
+	
+	public static void hanoi(int n, int a, int b, int c) throws IOException{
+		if(n==1) {
+			sb.append(a+" "+c+"\n");
+			return;
+		} 
+		
+		hanoi(n-1,a,c,b);
+		sb.append(a+" "+c+"\n");
+		hanoi(n-1,b,a,c);
+	}
+	
 	public static void main(String[] args) throws IOException {
-		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
- 
-		int N = Integer.parseInt(br.readLine());
- 
-		bw.write((int) (Math.pow(2, N) - 1) + "\n");
- 
-		Hanoi(N, 1, 2, 3);
+		
+		int num = Integer.parseInt(br.readLine());
+		
+		// StringBuilder 사용법
+		bw.write((int)(Math.pow(2,num)-1)+"\n");
+		hanoi(num, 1, 2, 3);
 		bw.write(sb+"\n");
 		
+		br.close();
 		bw.flush();
 		bw.close();
-	}
- 
-	public static void Hanoi(int N, int start, int mid, int to) throws IOException {
-		if (N == 1) {
-			sb.append(start + " " + to + "\n");
-			return;
-		}
- 
-		// A -> C로 옮긴다고 가정할 떄,
-		// STEP 1 : N-1개를 A에서 B로 이동 (= start 지점의 N-1개의 원판을 mid 지점으로 옮긴다.)
-		Hanoi(N - 1, start, to, mid);
-		// STEP 2 : 1개를 A에서 C로 이동 (= start 지점의 N번째 원판을 to지점으로 옮긴다.)
-		sb.append(start + " " + to + "\n");
-		// STEP 3 : N-1개를 B에서 C로 이동 (= mid 지점의 N-1개의 원판을 to 지점으로 옮긴다.)
-		Hanoi(N - 1, mid, start, to);
- 
 	}
 
 }
