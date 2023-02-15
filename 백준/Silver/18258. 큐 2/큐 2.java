1a8f4aaca966c9138a8daa2a2253be27277ff505
박현도 <117895948+hyundo0328@users.noreply.github.com>
2023-02-15 21:38:37 +0900

    [Silver IV] Title: 큐 2, Time: 1508 ms, Memory: 407896 KB -BaekjoonHub

--- 백준/Silver/18258. 큐 2/큐 2.java
+++ 백준/Silver/18258. 큐 2/큐 2.java
@@ -3,53 +3,58 @@ import java.io.BufferedWriter;
 import java.io.IOException;
 import java.io.InputStreamReader;
 import java.io.OutputStreamWriter;
+import java.util.ArrayDeque;
 import java.util.ArrayList;
+import java.util.Deque;
+import java.util.LinkedList;
+import java.util.Queue;
 
 public class Main {
 
 	public static void main(String[] args) throws IOException {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 		BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+
+		LinkedList<Integer> queue = new LinkedList<>();
+		// Deque<Integer> qu = new ArrayDeque<>();
 		
 		int count = Integer.parseInt(br.readLine());
-		ArrayList<Integer> stack = new ArrayList<>();
-		int front = 0;
-		// int back = -1;
+		// int n = 0;
 		
 		for(int i=0;i<count;i++) {
 			String str[] = br.readLine().split(" ");
 			String order = str[0];
+			
 			if(str[0].equals("push")) {
 				int n = Integer.parseInt(str[1]);
 				// back++;
-				stack.add(n);
+				queue.offer(n);
+				// queue.push는 앞으로 삽입
 			} else if(str[0].equals("pop")) {
-				if(stack.size()==0 || front>=stack.size()) {
+				if(queue.size()==0) {
 					bw.write("-1\n");
 				} else {
-					bw.write(stack.get(front)+"\n");
-					// stack.remove(front);
-					front++;
+					bw.write(queue.poll()+"\n");
 				}
 			} else if(str[0].equals("size")) {
-				bw.write(stack.size()-front+"\n");
+				bw.write(queue.size()+"\n");
 			} else if(str[0].equals("empty")) {
-				if(stack.size()-front == 0) {
-					bw.write(1+"\n");
+				if(queue.isEmpty()) {
+					bw.write("1\n");
 				} else {
-					bw.write(0+"\n");
+					bw.write("0\n");
 				}
 			} else if(str[0].equals("front")) {
-				if(stack.size()==0 || front>=stack.size()) {
+				if(queue.size()==0) {
 					bw.write("-1\n");
 				} else {
-					bw.write(stack.get(front)+"\n");
+					bw.write(queue.peek()+"\n");
 				}
 			} else if(str[0].equals("back")) {
-				if(stack.size()==0 || front>=stack.size()) {
+				if(queue.size()==0) {
 					bw.write("-1\n");
 				} else {
-					bw.write(stack.get(stack.size()-1)+"\n");
+					bw.write(queue.getLast()+"\n");
 				}
 			}
 		}
