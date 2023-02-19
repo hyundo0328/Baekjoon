108c06eae19f1160a3e41056f780437a55381d99
박현도 <117895948+hyundo0328@users.noreply.github.com>
2023-02-20 00:30:33 +0900

    [Bronze I] Title: 수 정렬하기 3, Time: 1676 ms, Memory: 336808 KB -BaekjoonHub

--- 백준/Bronze/10989. 수 정렬하기 3/수 정렬하기 3.java
+++ 백준/Bronze/10989. 수 정렬하기 3/수 정렬하기 3.java
@@ -8,27 +8,36 @@ import java.util.Arrays;
 public class Main {
 
 	public static void main(String[] args) throws IOException {
-		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-		BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
-		
-		int num = Integer.parseInt(br.readLine());
-		int arr[] = new int[num];
-		// ArrayList<String> arr = new ArrayList<>();
-		
-		for(int i=0;i<num;i++) {
-			arr[i] = Integer.parseInt(br.readLine());
-		}
-		
-		Arrays.sort(arr);
-		// 배열을 사용할 때는 Arrays를 사용하여 sort를 하고 ArrayList를 사용할 때는 Collections를 사용하여 sort를 한다.
-		
-		for(int i=0;i<num;i++) {
-			bw.write(arr[i]+"\n");
-		}
-		
-		br.close();
-		bw.flush();
-		bw.close();
+		// 수의 범위 (0 ~ 10000) 사실상 0은 제외
+        int[] cnt = new int[10001];
+ 
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+ 
+        int N = Integer.parseInt(br.readLine());
+ 
+        for (int i = 0; i < N; i++) {
+            // 해당 인덱스의 값을 1 증가
+            cnt[Integer.parseInt(br.readLine())] ++;
+        }
+ 
+        br.close();
+ 
+        StringBuilder sb = new StringBuilder();
+ 
+        // 0은 입력범위에서 없으므로 1부터 시작
+        for(int i = 1; i < 10001; i++){
+            // i 값이 개수가 0 이 될 때 까지 출력 (빈도수를 의미)
+            while(cnt[i] > 0){
+                sb.append(i).append('\n');
+                cnt[i]--;
+            }
+        }
+        System.out.println(sb);
+        
+        br.close();
+        bw.flush();
+        bw.close();
 	}
 
 }
