6012e84d095177ed06e4e4088fe078a14b77be3d
박현도 <117895948+hyundo0328@users.noreply.github.com>
2023-08-13 01:19:36 +0900

    [Bronze I] Title: 약수, Time: 152 ms, Memory: 15896 KB -BaekjoonHub

---
+++ 백준/Bronze/1037. 약수/약수.java
@@ -0,0 +1,33 @@
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.StringTokenizer;
+
+public class Main {
+
+	public static void main(String[] args) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+		StringTokenizer st;
+		
+		long count = Long.parseLong(br.readLine());
+		ArrayList<Long> num = new ArrayList<Long>();
+		st = new StringTokenizer(br.readLine());
+		while(st.hasMoreTokens()) {
+			num.add(Long.parseLong(st.nextToken()));
+		}
+		
+		Collections.sort(num);
+		
+		bw.write(num.get(0)*num.get(num.size()-1)+"\n");
+		
+		br.close();
+		bw.flush();
+		bw.close();
+	}
+
+}
