efad819225f1e88c70f2c540da3f02218fd48b80
박현도 <117895948+hyundo0328@users.noreply.github.com>
2023-08-30 15:03:46 +0900

    Update 블랙잭.java

--- 백준/Bronze/2798. 블랙잭/블랙잭.java
+++ 백준/Bronze/2798. 블랙잭/블랙잭.java
@@ -3,7 +3,6 @@ import java.io.BufferedWriter;
 import java.io.IOException;
 import java.io.InputStreamReader;
 import java.io.OutputStreamWriter;
-import java.util.ArrayList;
 import java.util.StringTokenizer;
 
 public class Main {
@@ -11,42 +10,36 @@ public class Main {
 	public static void main(String[] args) throws IOException {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 		BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
-		StringTokenizer st;
+		StringTokenizer st = new StringTokenizer(br.readLine());
 		
-		st = new StringTokenizer(br.readLine()," ");
 		int n = Integer.parseInt(st.nextToken());
 		int m = Integer.parseInt(st.nextToken());
 		
-		ArrayList<Integer> arr = new ArrayList<>();
-		
-		st = new StringTokenizer(br.readLine()," ");
+		int arr[] = new int[n];
+		st = new StringTokenizer(br.readLine());
 		for(int i=0;i<n;i++) {
-			arr.add(Integer.parseInt(st.nextToken()));
+			arr[i] = Integer.parseInt(st.nextToken());
 		}
 		
 		int sum = 0;
-		int tmp = 0;
-		int count = 0;
-		for(int i=0;i<n;i++) {
-			tmp += arr.get(i);
-			for(int j=i+1;j<n;j++) {
-				tmp += arr.get(j);
-				for(int k=j+1;k<n;k++) {
-					tmp += arr.get(k);
-					
-					if(0<=m-tmp && m-tmp<=m-sum) {
-						sum = tmp;
-						// bw.write(arr.get(i)+" "+arr.get(j)+" "+arr.get(k)+"\n");
+		int answer = 0;
+		for(int i=0;i<n-2;i++) {
+			for(int j=1;j<n-1;j++) {
+				for(int k=2;k<n;k++) {
+					if(i==j||i==k||j==k) {
+						continue;
+					} else {
+						sum = arr[i]+arr[j]+arr[k];
+						
+						if(answer < sum && sum <= m) {
+							answer = sum;
+						}
 					}
-					
-					tmp -= arr.get(k);
 				}
-				tmp -= arr.get(j);
 			}
-			tmp -= arr.get(i);
 		}
 		
-		bw.write(sum+"");
+		bw.write(answer+"\n");
 		
 		br.close();
 		bw.flush();
